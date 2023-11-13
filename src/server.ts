5b4ba3bc59c71d2e58d764c2af631c0f38df31de
niv netanel <nivnetanel@gmail.com>
2023-11-13 02:08:47 +0200

    Add origin cors

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "development") {
 
 // cors
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://niv-shopping-list.netlify.app/, http://localhost:3000");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
